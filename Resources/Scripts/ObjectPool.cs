c097e9c0814274983a1f625cc281c224fc64391a
zxycode007 <373982141@qq.com>
2017-11-16 22:04:21 +0800

    增加对象池

--- Resources/Scripts/ObjectPool.cs
+++ Resources/Scripts/ObjectPool.cs
@@ -1,6 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 
+public class ObjectPool
+{
+    public string objectName { get; protected set; }
+    public Type objectType { get; protected set; }
+
+    public virtual object GetObject();
+
+    public virtual void   Recycle(object obj);
+    
+}
+public class ObjectPoolImpl<T>  : ObjectPool where T : new()
+{
+
+}
  
+public class ObjecPoolManager
+{
+
+     
+
+}
